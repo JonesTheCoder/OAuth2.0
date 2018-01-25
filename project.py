d861859767926f8691b0253954bd1b34e5f18664
vagrant <vagrant@vagrant.vm>
2018-01-25 17:36:44 +0000

    fixed small bug in getUserID

--- project.py
+++ project.py
@@ -122,7 +122,7 @@ def gconnect():
     login_session['email'] = data['email']
 
     # See if a user exists, if it doesn't make a new one
-    user_id = getUserID(login_session)['email'])
+    user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
