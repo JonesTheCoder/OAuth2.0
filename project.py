c75cf7ff8fa064e7fb7835c89d4642bfcdfb302e
vagrant <vagrant@vagrant.vm>
2018-01-25 17:35:11 +0000

    Added get user id code from ucacity

--- project.py
+++ project.py
@@ -121,6 +121,12 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+    # See if a user exists, if it doesn't make a new one
+    user_id = getUserID(login_session)['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
